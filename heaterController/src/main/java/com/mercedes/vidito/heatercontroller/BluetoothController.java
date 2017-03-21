550426f0a9d2db83de9df1083b94034f0e61dc7c
Vidina Oliva Rodríguez <oliva5.rguez@googlemail.com>
2017-03-21 17:58:11 +0100

    v1.2 Connection with Bluetooth Device

--- heaterController/src/main/java/com/mercedes/vidito/heatercontroller/BluetoothController.java
+++ heaterController/src/main/java/com/mercedes/vidito/heatercontroller/BluetoothController.java
@@ -1,8 +1,14 @@
 package com.mercedes.vidito.heatercontroller;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.widget.Toast;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+
+import java.io.IOException;
+import java.util.UUID;
+
 
 /**
  * Created by vidina.oliva on 21.03.2017.
@@ -10,18 +16,62 @@ import android.widget.Toast;
 
 public class BluetoothController extends Object {
 
+    private static final UUID MY_UUID = UUID.fromString("2320f898-0e52-11e7-93ae-92361f002671");
     private String bluetoothAddress;
+    private Activity mainActivity;
     private BluetoothAdapter mBluetoothAdapter;
 
-    {
-        bluetoothAddress = "98:4F:EE:0F:4F:E7";
+
+    public BluetoothController(Activity mainActivity) {
+        this.mainActivity = mainActivity;
+        this.bluetoothAddress = "98:4F:EE:0F:4F:E7";
+        this.mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
-    public BluetoothAdapter getBluetoothAdapter () {
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        return mBluetoothAdapter;
+    public boolean existBluetoothAdapter() {
+
+        if (mBluetoothAdapter != null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+
+    public void enableAdapter() {
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            mainActivity.startActivityForResult(enableBtIntent, Constants.REQUEST_ENABLE_BT);
+        }
+    }
+
+
+    public boolean connect() {
+        BluetoothDevice arduino = mBluetoothAdapter.getRemoteDevice(bluetoothAddress);
+        BluetoothSocket tmp = null;
+        if (arduino != null) {
+            try {
+                tmp = arduino.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException ex) {
+                return false;
+            }
+            try {
+                tmp.connect();
+            } catch (IOException e) {
+                try {
+                    tmp.close();
+                    return false;
+                } catch (IOException e1) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 
 
 }
--- heaterController/src/main/java/com/mercedes/vidito/heatercontroller/MainScreen.java
+++ heaterController/src/main/java/com/mercedes/vidito/heatercontroller/MainScreen.java
@@ -6,36 +6,66 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainScreen extends AppCompatActivity {
-
-    private BluetoothController bc;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainscreen);
-
+        final BluetoothController bluetoothController = new BluetoothController(this);
         final Button connectButton = (Button) findViewById(R.id.button_connect);
         connectButton.setOnClickListener(new View.OnClickListener(){
                 public void onClick(View view){
-                    connectToArduino();
+                    enableAdapter(bluetoothController);
+                    connectToArduino(bluetoothController);
                 }
 
         });
 
     }
 
-    public void connectToArduino(){
-        showMessage("Hola Que tal?");
+    private void enableAdapter(BluetoothController controller){
+        if(controller.existBluetoothAdapter()){
+            controller.enableAdapter();
+        } else {
+            showMessage("Bluetooth adapter does not exists");
+            finish();
+        }
+
+
     }
 
-    public void showMessage (String message){
+    private void connectToArduino(BluetoothController controller){
+        if (controller.connect()){
+            showMessage("Connection establish with Arduino");
+        } else {
+            showMessage("Could not connect to Arduino");
+            finish();
+        }
+
+    }
+
+    private void showMessage(String message){
         Context context = getApplicationContext();
-        int duration = Toast.LENGTH_SHORT;
+        int duration = Toast.LENGTH_LONG;
         Toast toast = Toast.makeText(context, message, duration);
         toast.show();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == Constants.REQUEST_ENABLE_BT && resultCode == RESULT_CANCELED) {
+            showMessage("Could not enable Bluetooth");
+            finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
 }
